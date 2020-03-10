b3c126222b1a8f3e8a09df5b3825011deb131824
THE JOKER <pavel11_06@mail.ru>
2020-03-10 14:42:34 +0800

    comments

--- index.go
+++ index.go
@@ -10,21 +10,34 @@ import (
 
 var tasks map[string]*models.Task
 
+/**
+ * Метод генерации уникального идентификатора
+ * задачи
+ *
+ * Возвращает случайно сгенерируемую 16-байтную строку
+ */
 func GenerateId() string {
     b := make([]byte, 16)
     rand.Read(b)
     return fmt.Sprintf("%x", b)
 }
 
+/**
+ * Главная страница ToDoList
+ *
+ */
 func indexHandler(w http.ResponseWriter, r *http.Request) {
     t, err := template.ParseFiles("views/index.html", "templates/header.html", "templates/footer.html")
     if err != nil {
         fmt.Fprintf(w, err.Error())
     }
-    fmt.Println(tasks)
     t.ExecuteTemplate(w, "index", tasks)
 }
 
+/**
+ * Страница создания новой задачи в ToDoList
+ *
+ */
 func createHandler(w http.ResponseWriter, r *http.Request) {
     t, err := template.ParseFiles("views/form.html", "templates/header.html", "templates/footer.html")
     if err != nil {
@@ -33,6 +46,10 @@ func createHandler(w http.ResponseWriter, r *http.Request) {
     t.ExecuteTemplate(w, "form", nil)
 }
 
+/**
+ * Страница редактирования задачи в ToDoList
+ *
+ */
 func updateHandler(w http.ResponseWriter, r *http.Request) {
     t, err := template.ParseFiles("views/form.html", "templates/header.html", "templates/footer.html")
     if err != nil {
@@ -46,12 +63,19 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
     t.ExecuteTemplate(w, "form", task)
 }
 
+/**
+ * Метод сохранения данных задачи
+ *
+ * Считываем отправленые дынные из формы
+ * Если уникальный идентификатор не пустой -
+ * редактируем задачу, иначе - создаем
+ */
 func saveHandler(w http.ResponseWriter, r *http.Request) {
     id := r.FormValue("id")
     title := r.FormValue("title")
     description := r.FormValue("description")
-
     var task *models.Task
+
     if id != "" {
         task = tasks[id]
         task.Title = title
@@ -65,6 +89,10 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, "/", 302)
 }
 
+/**
+ * Метод удаления задачи из ToDoList
+ *
+ */
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
     id := r.FormValue("id")
     if id == "" {
@@ -75,13 +103,38 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
     http.Redirect(w, r, "/", 302)
 }
 
+/**
+ * Основной метод ToDoList
+ * Метод инициализации программы
+ *
+ */
 func main() {
+
+	/**
+	 * Инициализация пустой карты с задачами
+	 *
+	 */
     tasks = make(map[string]*models.Task, 0)
+
+    /**
+	 * Доступ к ресурсам программы
+	 *
+	 */
     http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+
+    /**
+	 * Роутинг URL
+	 *
+	 */
     http.HandleFunc("/", indexHandler)
     http.HandleFunc("/create", createHandler)
     http.HandleFunc("/update", updateHandler)
     http.HandleFunc("/delete", deleteHandler)
     http.HandleFunc("/save", saveHandler)
+
+    /**
+	 * Старт сервера на 8181-порту
+	 *
+	 */
     http.ListenAndServe(":8181", nil)
 }
--- models/Task.go
+++ models/Task.go
@@ -1,11 +1,23 @@
 package models
 
+/**
+ * Task - модель, представляющая задачу в ToDoList
+ *
+ * Id - уникальный идентификатор, генерируемый
+ * отдельной функцией GenerateId()
+ * Title - заголовок задачи в ToDoList
+ * Description - описание задачи в ToDoList
+ */
 type Task struct {
     Id          string
     Title       string
     Description string
 }
 
+/**
+ * NewTask - конструктор структуры Task,
+ * возвращающий объект задачи
+ */
 func NewTask(id, title, description string) *Task {
     return &Task{id, title, description}
 }
